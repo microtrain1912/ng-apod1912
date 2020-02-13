78d5f5e517d0e1266ea314af8fb0cc631156b539
chad svastisalee <swasgrafx@gmail.com>
2020-02-12 20:42:32 -0600

    Create a random date for each successful APOD

--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -12,6 +12,8 @@ import { Apod } from '../models/apod';
 export class ApodComponent implements OnInit {
 
   apod:Apod;
+  //1. Create date as an instance variable
+  date:string;
 
   constructor(
     private apodService: ApodService,
@@ -29,16 +31,34 @@ export class ApodComponent implements OnInit {
   //4. Replace the current date with an updated method signature
   getApod(date:string): void{
 
+    //If the date is falsy, use today's date
+    if(!date){
+      date = new Date().toISOString().slice(0,10);
+    }
+
+
     this.apodService.getApod(date).subscribe(
       (response:any)=>{
         this.apod = response;
-
-        //5. Log the results to the JS console
-        console.log(response);
+        //3.  Update this.date on each API call
+        this.date = this.randomDate(new Date(1995,5,16), new Date());
+        //4. Log this.date to the JS console
+        console.log(this.date);
       }
     );
 
   }
 
+  //2. Create a method that returns a random date
+  randomDate(start, end): string{
+    let date = new Date(
+      start.getTime() + Math.random() *
+        (end.getTime() - start.getTime())
+    );
+
+    return new Date(date).toISOString().slice(0,10);
+  }
+
 }
+
 
